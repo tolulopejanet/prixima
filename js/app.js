70d79ea7f130539b864eaa08a50dc391a32d9575
tolulope <tolulopefaleyejanet@gmail.com>
2022-09-15 11:13:52 +0100

    This is a completed bootstrap project

---
+++ js/app.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+        // hero slider
+
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed:1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        navText: ["PREV", "NEXT"],
+        responsive:{
+            0:{ 
+                nav:false
+            },
+            768:{
+                nav:true
+            }
+        }
+    });
+
+     // project slider
+
+     $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: false,
+        smartSpeed:1000,
+        margin:24,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+                margin:0,
+            },
+            768:{
+                items:2,
+            },
+            1140:{
+                items:2,
+                center:true,
+                dots:true,
+            }
+        }
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: true,
+        items:1,
+        smartSpeed:800,
+        
+    }) 
+});
